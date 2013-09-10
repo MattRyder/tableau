9e8d195e5c3cbb6e58cacfbc082ae42c0c440d38
Matt Ryder <matt@mattryder.co.uk>
2013-09-10 21:55:20 +0100

    update UriBuilder to work for Student Set timetables, as well as Module lookups

--- lib/tableau/uribuilder.rb
+++ lib/tableau/uribuilder.rb
@@ -3,21 +3,28 @@ module Tableau
 
     def defaults
       @options = {
-        root: 'crwnmis3.staffs.ac.uk/Reporting/',
-        timetable_type: 'Individual',
-        timetable_template: 'Module%20Individual%20SOC',
-        weeks:           '10-25',
-        days:            '1-5', # Mon - Fri
-        period_from:     '5',  # 15 min intervals since 8AM
-        period_to:       '52',
-        optional_params: '&width=0&height=0' #optional params
+        root:               'crwnmis3.staffs.ac.uk/Reporting/',
+        timetable_type:     'Individual',
+        lookup_type:        'Student+Sets',
+        timetable_template: 'Design+Template',
+        weeks:              '10-25',                        # 10-25 for Semester 1, 26-42 for Semester 2
+        days:               '1-5',                          # Mon - Fri
+        period_from:        '5',                            # 15 min intervals since 8AM
+        period_to:          '52',
+        optional_params:    '&width=0&height=0'             #optional params
       }
     end
 
-    def initialize(module_id, semester, options = {})
-      @module_id = module_id
+    def initialize(lookup_id, options = {})
+      @lookup_id = lookup_id
       defaults.merge!(options)
-      @options[:weeks] = semester == 1 ? '10-25' : '26-42'
+
+      if options[:module_lookup]
+        @options[:lookup_type] = "Modules"
+        @options[:timetable_template] = "Module%20Individual%20SOC"
+      end
+
+      @options[:weeks] = ENV["TABLEAU_SEMESTER"] == 1 ? '10-25' : '26-42'
     end
 
     def read
@@ -25,11 +32,11 @@ module Tableau
     end
 
     def to_s
-      "http://#{@options[:root]}#{@options[:timetable_type]};Modules;name;" +
-      "#{@module_id}?&template=#{@options[:timetable_template].gsub(' ', '%20')}"+
-      "&weeks=#{@options[:weeks]}&days=#{@options[:days]}"+
-      "&periods=#{@options[:period_from]}-#{@options[:period_to]}"+
-      "#{@options[:optional_params]}"
+        "http://#{@options[:root]}#{@options[:timetable_type]};#{@options[:lookup_type]};name;" +
+        "#{@lookup_id}?&template=#{@options[:timetable_template].gsub(' ', '%20')}" +
+        "&weeks=#{@options[:weeks]}&days=#{@options[:days]}" +
+        "&periods=#{@options[:period_from]}-#{@options[:period_to]}" +
+        "#{@options[:optional_params]}"
     end
 
   end
--- spec/uribuilder_spec.rb
+++ spec/uribuilder_spec.rb
@@ -2,15 +2,35 @@ require 'spec_helper'
 
 describe 'UriBuilder' do
 
-  before do
-    @uribuilder = Tableau::UriBuilder.new('CE00758-5', 2)
+  context 'looking up a timetable set' do
+
+    before do
+      @uribuilder = Tableau::UriBuilder.new('l3cs')
+    end
+
+    it 'should return a correct URI' do
+      @uribuilder.to_s.should eq(
+        %Q{http://crwnmis3.staffs.ac.uk/Reporting/Individual;Student+Sets;name;l3cs?&template=Design+Template&weeks=26-42&days=1-5&periods=5-52&width=0&height=0})
+    end
+
+    it 'should return a non-zero IO Class' do
+      @uribuilder.read.class.should eq(String)
+    end
   end
 
-  it 'should return an IO class of read data' do
-  end
+  context 'looking up a module' do
 
+    before do
+      @uribuilder = Tableau::UriBuilder.new('CE00758-5', module_lookup: true)
+    end
 
-  it 'should return a correct URI' do
-    @uribuilder.to_s.should eq("http://crwnmis3.staffs.ac.uk/Reporting/Individual;Modules;name;CE00758-5?&template=Module%20Individual%20SOC&weeks=26-42&days=1-5&periods=5-52&width=0&height=0")
+    it 'should return a correct URI' do
+      @uribuilder.to_s.should eq(
+        %Q{http://crwnmis3.staffs.ac.uk/Reporting/Individual;Modules;name;CE00758-5?&template=Module%20Individual%20SOC&weeks=26-42&days=1-5&periods=5-52&width=0&height=0})
+    end
+
+    it 'should return a non-zero IO Class' do
+      @uribuilder.read.class.should eq(String)
+    end
   end
 end
