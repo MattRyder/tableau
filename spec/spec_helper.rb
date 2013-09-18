0ab0871ea4de06592d06272a75055284df391b24
Matt Ryder <matt@mattryder.co.uk>
2013-09-18 23:24:52 +0100

    remove fixtures, fix Travis Sqlite3?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,8 +30,6 @@ Spork.prefork do
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
   RSpec.configure do |config|
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
